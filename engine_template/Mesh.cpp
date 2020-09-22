6f15644b498ce58b71ba78e0d147e791cef2c683
Grant Hernandez <grant.h.hernandez@gmail.com>
2020-09-21 20:17:15 -0700

    Make engine compile with new GUI gate

--- engine_template/Mesh.cpp
+++ engine_template/Mesh.cpp
@@ -22,7 +22,7 @@ bool Mesh::init()
   //vao_ = make_plane(1.0, 1.0);
   vao2_ = make_grid(50, 50, 40.0, 40.0, {0.0, 0.0, 0.0});
   vao_ = make_grid(50, 50, 40.0, 40.0, {1.0, 1.0, 1.0}, true);
-  vao2_ = make_cube();//make_grid(50, 50, 40.0, 40.0, {1.0, 1.0, 1.0}, true);
+  //vao2_ = make_cube();//make_grid(50, 50, 40.0, 40.0, {1.0, 1.0, 1.0}, true);
   //vao2_ = make_axis(5.0);
   //
   shader_ = ResourceManager::instance()->get_shader_program("Phong");
@@ -52,13 +52,15 @@ void Mesh::draw(std::shared_ptr<CameraObject> camera)
   static bool usetex = true;
   ImVec4 light_pos(sin(glfwGetTime())*5.0f, 1.0f,cos(glfwGetTime())* 5.0f, 0.0f);
 
-  ImGui::Begin("Slider");
-  ImGui::Checkbox("usetex", &usetex);
-  ImGui::SliderFloat("distance", &f1, -1.0f, 20.0f, "distance = %.3f");
-  ImGui::SliderFloat("falloff", &f2, 0.0, 1.0f, "falloff = %.3f");
-  ImGui::SliderFloat("mod", &mod, 0.0, 5.0f, "mod = %.3f");
-  ImGui::ColorEdit3("light color", (float*)&light_color);
-  ImGui::End();
+  DGUI_BEGIN;
+    ImGui::Begin("Slider");
+    ImGui::Checkbox("usetex", &usetex);
+    ImGui::SliderFloat("distance", &f1, -1.0f, 20.0f, "distance = %.3f");
+    ImGui::SliderFloat("falloff", &f2, 0.0, 1.0f, "falloff = %.3f");
+    ImGui::SliderFloat("mod", &mod, 0.0, 5.0f, "mod = %.3f");
+    ImGui::ColorEdit3("light color", (float*)&light_color);
+    ImGui::End();
+  DGUI_END;
 
   shader_->set_uniform("Mod", mod);
   shader_->set_uniform("light", 0, "position", glm::vec3(light_pos.x, light_pos.y, light_pos.z));
